c69cccea3ba0a9559a0dad4b44e03076cb94ebc7
ruokua <ruokua@ougl-04-a.eplt.washington.edu>
2015-04-18 14:24:27 -0700

    finish onCreate

--- app/src/main/java/edu/washington/ruokua/lifecounter/MainActivity.java
+++ app/src/main/java/edu/washington/ruokua/lifecounter/MainActivity.java
@@ -1,19 +1,44 @@
 package edu.washington.ruokua.lifecounter;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends ActionBarActivity {
+    public final int DEFAULT_NUM_PLAYER = 2;
+    public final int DEFAULT_LIFE = 20;
+
+    private ArrayList<Integer> playerInfo = new ArrayList<Integer>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        if (savedInstanceState == null) {
 
+            for (int i = 0; i < DEFAULT_NUM_PLAYER; i++) {
+                 playerInfo.add(DEFAULT_LIFE);
+                 //Get the layout
+                 String playerName = "player" + (i + 1);
+                 int playerLayoutID = getResources().getIdentifier(playerName, "id", getPackageName());
+                 LinearLayout layout = (LinearLayout) findViewById(playerLayoutID);
+                 //Set the Name label
+                 TextView nameView = (TextView) layout.findViewById(R.id.player_name);
+                 nameView.setText(playerName + ": ");
+                 //Set the health label
+                 TextView healthView = (TextView) layout.findViewById(R.id.player_life);
+                 healthView.setText("" + playerInfo.get(i)); //Set the health to value from arraylist
+
+            }
+
+       }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
